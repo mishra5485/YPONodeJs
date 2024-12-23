b73f660b4693d0ab8df7221aab69f968aa446ff8
Harsh Mishra <Harshmishra5485@gmail.com>
2024-12-23 13:08:08 +0530

    Removed the cluster Module

--- index.js
+++ index.js
@@ -5,63 +5,49 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
-import cluster from "cluster";
-import os from "os";
 import bodyParser from "body-parser";
 
 import { Chapter, Users } from "./routes/AdminRoutes/index.js";
 
 dotenv.config();
 
-const numCPUs = os.cpus().length;
+connectToDatabase();
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
+const app = express();
 
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+// Middleware
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died, restarting...`);
-    cluster.fork();
-  });
-} else {
-  connectToDatabase();
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    credentials: true,
+  })
+);
 
-  const app = express();
-  app.use(express.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
+// Set view engine and static files
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-  app.use(
-    cors({
-      origin: "*",
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
-      credentials: true,
-    })
-  );
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-  // Set view engine and static files
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
+app.use("/chapter", Chapter);
+app.use("/user", Users);
 
-  app.set("view engine", "ejs");
-  app.set("views", path.join(__dirname, "views"));
+app.use("/uploads", express.static("uploads"));
+app.use("/Assets", express.static("Assets"));
 
-  app.use("/chapter", Chapter);
-  app.use("/user", Users);
+app.use("*", (req, res) => {
+  res.status(404).render("404");
+});
 
-  app.use("/uploads", express.static("uploads"));
-  app.use("/Assets", express.static("Assets"));
-
-  app.use("*", (req, res) => {
-    res.status(404).render("404");
-  });
-
-  const port = config.backendPort || 3000;
-  app.listen(port, () => {
-    console.log(`Worker ${process.pid} is listening on port ${port}`);
-  });
-}
+// Start the server
+const port = config.backendPort || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
